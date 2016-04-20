0df8638955ee81c5a2e63d7ae5fafff6b1e1191b
abattaje <Akshay Battaje>
2016-04-20 16:59:41 -0400

    Threaded receiver

--- data_transfer/receiver.py
+++ data_transfer/receiver.py
@@ -2,41 +2,72 @@
 
 import socket
 import json
-#from nmap_util import get_active_ips
+import threading
+import sys
 
 PORT = 49999
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+class Client(threading.Thread):
+    def __init__(self, (client_conn, client_addr)):
+        threading.Thread.__init__(self)
+        self.client_conn = client_conn
+        self.client_addr = client_addr
+        self.size = 2048
 
-def start_server():
-    dest_addr = (get_ip_address(), 49999)
-    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_sock.bind(dest_addr)
-    tcp_sock.listen(1)
+    def run(self):
+        while True:
+            data = None
+            try:
+                data = self.client_conn.recv(self.size)
+            except socket.error, e:
+                print(e.message)
 
-    while True:
-        data = None
-        connection, client_addr = tcp_sock.accept()
+            if data is not None:
+                try:
+                    data = json.loads(data)
+                    print("Received : " + str(data))
+                    if 'mydata' in data:
+                        print(data['mydata'])
+                except ValueError:
+                    continue
+
+            self.client_conn.close()
+            break
+
+class Receiver:
+    def __init__(self):
+        self.host = self.get_ip_address()
+        self.port = 49999
+        self.threads = list()
+        self.tcp_sock = None
+
+    def get_ip_address(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+
+
+    def create_socket(self):
         try:
-            data = connection.recv(2048)
-        except socket.error, e:
-            print(e.message)
+            self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.tcp_sock.bind((self.host, self.port))
+            self.tcp_sock.listen(2)
+        except socket.error:
+            if self.tcp_sock:
+                self.tcp_sock.close()
+            print("Failure to open socket")
+            sys.exit(1)
 
-        if data is not None:
-            data = json.loads(data)
-            print("Received : " + str(data))
-            if 'mydata' in data:
-                print(data['mydata'])
-
-        connection.close()
-
+    def run(self):
+        self.create_socket()
+        while True:
+            client = Client(self.tcp_sock.accept())
+            client.start()
+            self.threads.append(client)
 
 def main():
-    active_ips = get_active_ips()
-    start_server()
+    receiver = Receiver()
+    receiver.run()
 
 if __name__ == '__main__':
     main()
