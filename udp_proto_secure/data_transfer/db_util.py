733db8592e24708714406243a4b711618dd99f00
Deepanshu Lulla <deepanshu.lulla@gmail.com>
2016-04-22 01:29:27 -0400

    added tabs and changed db_utils

--- udp_proto_secure/data_transfer/db_util.py
+++ udp_proto_secure/data_transfer/db_util.py
@@ -2,114 +2,122 @@ from pymongo import MongoClient
 import os
 
 class DBUtil:
-    def __init__(self):
-        self.len_of_mac = 12
-        self.operation = {'insert' : 0, 'delete' : 1}
-        self.command_beginning = "sudo python ovxctl.py -n "
-        self.ovx_dir = "/home/ubuntu/OpenVirteX/utils"
+	def __init__(self):
+		self.len_of_mac = 12
+		self.operation = {'insert' : 0, 'delete' : 1}
+		self.command_beginning = "sudo python ovxctl.py -n "
+		self.ovx_dir = "/home/ubuntu/OpenVirteX/utils"
 
     # Insert colon into the string to make it a proper format
-    def insert_colons_in_string(self, inp_str):
-        return (':'.join(inp_str[i] +
-                         inp_str[i+1]
-                         for i in xrange(0, len(inp_str), 2)))
+	def insert_colons_in_string(self, inp_str):
+		return (':'.join(inp_str[i] + inp_str[i+1] for i in xrange(0, len(inp_str), 2)))
 
 
     # Convert the integer value of MAC to its hexadecimal value in appropriate format
-    def convert_mac_format(self, mac):
-        hex_value = str(hex(int(mac)))
-        hex_value = hex_value.replace("0x", "")
-        prefix_string = "0" * (self.len_of_mac - len(hex_value))
-        hex_value = prefix_string + hex_value
+	def convert_mac_format(self, mac):
+		hex_value = str(hex(int(mac)))
+		hex_value = hex_value.replace("0x", "")
+		prefix_string = "0" * (self.len_of_mac - len(hex_value))
+		hex_value = prefix_string + hex_value
 
-        return self.insert_colons_in_string(hex_value)
+		return self.insert_colons_in_string(hex_value)
 
 
     # Convert the integer value of DPID to its hexadecimal value in appropriate format
-    def convert_dpid_format(self, dpid):
-        hex_value = str(hex(int(dpid)))
-        hex_value = hex_value.replace("0x", "00")
-        return self.insert_colons_in_string(hex_value)
+	def convert_dpid_format(self, dpid):
+		hex_value = str(hex(int(dpid)))
+		hex_value = hex_value.replace("0x", "00")
+		return self.insert_colons_in_string(hex_value)
 
-    # Get the mac address of all the hosts for this tenant
-    def get_all_hosts_for_tenant(self, tenantId):
-        list_of_host_macs = dict()
-        is_tenant_present = False
-        connection = MongoClient('mongodb://localhost:27017/')
-        db = connection['OVX']
-        collection = db['VNET']
+	# Get the mac address of all the hosts for this tenant
+	def get_all_hosts_for_tenant(self, tenantId):
+		list_of_host_macs = dict()
+		is_tenant_present = False
+		connection = MongoClient('mongodb://localhost:27017/')
+		db = connection['OVX']
+		collection = db['VNET']
 
-        cursor = collection.find({'tenantId' : tenantId}, {'hosts' : 1})
-        obj = next(cursor, None)
+		cursor = collection.find({'tenantId' : tenantId}, {'hosts' : 1})
+		obj = next(cursor, None)
 
-        if obj is not None:
-            is_tenant_present = True
-            if 'hosts' in obj:
-                hosts = obj['hosts']
-                for host in hosts:
-                    if 'mac' in host:
-                        mac_addr = host['mac']
-                        list_of_host_macs[self.convert_mac_format(mac_addr)] = host
+		if obj is not None:
+			is_tenant_present = True
+			if 'hosts' in obj:
+				hosts = obj['hosts']
+				for host in hosts:
+					if 'mac' in host:
+						mac_addr = host['mac']
+						list_of_host_macs[self.convert_mac_format(mac_addr)] = host
 
-        return list_of_host_macs, is_tenant_present
-
-
-    def update_database(self, data):
-        tenantId = data['tenantId']
-        receiver_hosts, is_tenant_present = self.get_all_hosts_for_tenant(tenantId)
-
-        if not is_tenant_present:
-            return
-
-        sender_hosts = dict()
-        for host in data['hosts']:
-            sender_hosts[self.convert_mac_format(host['mac'])] = host
-
-        extra_in_sender = set(sender_hosts.keys()).difference(receiver_hosts.keys())
-        extra_in_receiver = set(receiver_hosts.keys()).difference(sender_hosts.keys())
-
-        # Insert
-        inserted = False
-        for host in extra_in_sender:
-            inserted = True
-	    print("Changed to : Inserted")
-            self.update_hosts(tenantId, sender_hosts[host], self.operation['insert'])
-
-        if not inserted:
-	    print("Not inserted. Deleting")
-            for host in extra_in_receiver:
-                self.update_hosts(tenantId, receiver_hosts[host], self.operation['delete'])
+		return list_of_host_macs, is_tenant_present
 
 
-    def update_hosts(self, tenantId, host, op):
-        commands = list()
-        stop_network = self.command_beginning + " stopNetwork " + str(tenantId)
-        commands.append(stop_network)
+	def update_database(self, data):
+		#tenantId = data['tenantId']
+		for tenantId,hosts in data.iteritems():
+			connection = MongoClient('mongodb://localhost:27017/')
+			db = connection['OVX']
+			collection = db['VNET']
 
-        command = ''
+			cursor = collection.find({'tenantId' : tenantId})
+			obj = next(cursor, None)
+			if obj is not None:
+				collection.update({'_id':obj['_id']},{'$set':{'hosts':hosts}})
+		'''		
+		receiver_hosts, is_tenant_present = self.get_all_hosts_for_tenant(tenantId)
 
-        if op == self.operation['insert']:
-            dpid = self.convert_dpid_format(host['vdpid'])
-            port = host['vport']
-            mac = self.convert_mac_format(host['mac'])
-            command = self.command_beginning + \
-                      " connectHost " + \
-                      str(tenantId) + " " + \
-                      str(dpid) + " " + \
-                      str(port) + " " + \
-                      str(mac)
-        else:
-            command = self.command_beginning + \
-                      " disconnectHost " + \
-                      str(tenantId) + " " + \
-                      str(host['hostId'])
+		if not is_tenant_present:
+			return
 
-        commands.append(command)
+		sender_hosts = dict()
+		for host in data['hosts']:
+			sender_hosts[self.convert_mac_format(host['mac'])] = host
 
-        start_network = self.command_beginning + "startNetwork " + str(tenantId)
-        commands.append(start_network)
+		#extra_in_sender = set(sender_hosts.keys()).difference(receiver_hosts.keys())
+		#extra_in_receiver = set(receiver_hosts.keys()).difference(sender_hosts.keys())
 
-        os.chdir(self.ovx_dir)
-        for cmd in commands:
-            tmp = os.popen(cmd).read()
-            print(tmp)
+		# Insert
+		inserted = False
+		for host in extra_in_sender:
+			inserted = True
+		print("Changed to : Inserted")
+			self.update_hosts(tenantId, sender_hosts[host], self.operation['insert'])
+
+		if not inserted:
+		print("Not inserted. Deleting")
+			for host in extra_in_receiver:
+				self.update_hosts(tenantId, receiver_hosts[host], self.operation['delete'])
+		'''
+
+	def update_hosts(self, tenantId, host, op):
+		commands = list()
+		stop_network = self.command_beginning + " stopNetwork " + str(tenantId)
+		commands.append(stop_network)
+
+		command = ''
+
+		if op == self.operation['insert']:
+			dpid = self.convert_dpid_format(host['vdpid'])
+			port = host['vport']
+			mac = self.convert_mac_format(host['mac'])
+			command = self.command_beginning + \
+					  " connectHost " + \
+					  str(tenantId) + " " + \
+					  str(dpid) + " " + \
+					  str(port) + " " + \
+					  str(mac)
+		else:
+			command = self.command_beginning + \
+					  " disconnectHost " + \
+					  str(tenantId) + " " + \
+					  str(host['hostId'])
+
+		commands.append(command)
+
+		start_network = self.command_beginning + "startNetwork " + str(tenantId)
+		commands.append(start_network)
+
+		os.chdir(self.ovx_dir)
+		for cmd in commands:
+			tmp = os.popen(cmd).read()
+			print(tmp)
--- udp_proto_secure/data_transfer/sender.py
+++ udp_proto_secure/data_transfer/sender.py
@@ -5,40 +5,41 @@ import json
 import os
 from cryptoLib import AesCtr,Hmac
 
-PORT = 49999
+
 
 
 class MessageSender:
-    def __init__(self,port):
+	def __init__(self,port):
 		self.port=port
-    def send_to_peers(self, active_ips, data):
 
-        for peer_ip in active_ips:
-            serv_addr = (peer_ip, PORT)
-            udp_sock = None
-            try:
-                udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                data1=json.dumps(data)		
-                
+	def send_to_peers(self, active_ips, data):
+
+		for peer_ip in active_ips:
+			serv_addr = (peer_ip, self.port)
+			udp_sock = None
+			try:
+				udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				data1=json.dumps(data)		
+				
 				enc_data,iv=encrypt(data1)
-                hmac_data=findHmac(enc_data)
+				hmac_data=findHmac(enc_data)
 				data_final=iv+"nonce"+enc_data+"nonce"+hmac_data
-                udp_sock.sendto(data_final, serv_addr)
-                udp_sock.close()
-            except Exception as e:
-                print(e)
-                udp_sock.close()
-                
+				udp_sock.sendto(data_final, serv_addr)
+				udp_sock.close()
+			except Exception as e:
+				print(e)
+				udp_sock.close()
+				
 
 
 
-    def findHmac(data):
-        hm=Hmac(data)
-        return hm.createHmac()
-        
-    def encrypt(data):
-        iv=os.urandom(16)
-        e1=AesCtr()
-        enc=e1.encryptData(data,iv)
+	def findHmac(data):
+		hm=Hmac(data)
+		return hm.createHmac()
+
+	def encrypt(data):
+		iv=os.urandom(16)
+		e1=AesCtr()
+		enc=e1.encryptData(data,iv)
 		return [enc,iv]
-        	
+		
